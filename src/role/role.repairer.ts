9ee4dca314c9ae7fcaf1dbf645621600fae3f1c6
KingOris <504419245@qq.com>
2022-04-28 16:44:43 +0900

    add repairer api and room extentions

--- src/role/role.repairer.ts
+++ src/role/role.repairer.ts
@@ -1,3 +1,62 @@
 /**
  * 维修者
  */
+
+ const repairer = (data: CreepData): CreepApi => ({
+    source: creep=>{
+        if(creep.store[RESOURCE_ENERGY] > 0){
+            return true
+        }
+
+        let sourceStructure: StructureStorage | StructureContainer | ERR_NOT_FOUND
+        if(!creep.memory.sourceId){
+            sourceStructure = creep.room.getAvaliblesource()
+            if(sourceStructure == ERR_NOT_FOUND){
+                creep.say('我傻了')
+                return false
+            }else{
+                creep.memory.sourceId = sourceStructure.id
+            }
+        }else{
+            sourceStructure = Game.getObjectById<StructureContainer | StructureStorage>(creep.memory.sourceId!)!
+        }
+
+        if (creep.withdraw(sourceStructure,RESOURCE_ENERGY) == ERR_NOT_ENOUGH_RESOURCES || creep.withdraw(sourceStructure,RESOURCE_ENERGY) == ERR_INVALID_TARGET){
+            creep.say('你是我滴神')
+            delete creep.memory.sourceId
+        }
+
+        if (creep.withdraw(sourceStructure,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+            creep.moveTo(sourceStructure,{visualizePathStyle: {stroke: '#ffffff'}})
+        }
+
+        return false
+    },
+
+    target: creep=>{
+        var targets = creep.room.getRepairstructure();
+        if(targets == ERR_NOT_FOUND){
+            creep.setFillWallid()
+            const result = creep.steadyWall()
+            if(result == ERR_NOT_FOUND){
+                if (creep.upgradeController(creep.room.controller!)== ERR_NOT_IN_RANGE){
+                    creep.moveTo(creep.room.controller!)
+                }
+            }
+        }else{
+            creep.memory.targetId = targets.id
+            if(creep.repair(targets) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets,{visualizePathStyle: {stroke: '#cc3fb5'}});
+            }
+        }
+
+        if(creep.store.getUsedCapacity() === 0 ){
+            return true
+        }
+
+        return false
+    }
+
+ })
+
+ export default repairer;
